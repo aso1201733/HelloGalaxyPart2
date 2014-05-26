25a81674f9abc902d9bee2979b74e27f8d4fb8d1
student <student@PC111S41.fgakusei.local>
2014-05-26 11:08:41 +0900

    ていしゅつ２

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.hellogalaxy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		Button OKbutton = (Button)findViewById(R.id.OKbutton);
+		OKbutton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		Log.v("aaaa","なった");
+		switch(v.getId()){
+		case R.id.OKbutton:
+			EditText etv = (EditText)findViewById(R.id.firstNameText);
+			 String inputMsg = etv.getText().toString();
+
+			 EditText etv2 = (EditText)findViewById(R.id.lastNameText);
+			 String inputMsg2 = etv2.getText().toString();
+
+			 TextView tv = (TextView)findViewById(R.id.resultLabel);
+			 tv.setText("あなたでしたか、"+ inputMsg + inputMsg2 +"さん。");
+		}
+
 	}
 
 	@Override
